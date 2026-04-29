4639482f5ce6e1056bb29b40265f9f9c9295fe40
YsnBnc <ysnbnc05@gmail.com>
2026-04-30 01:42:13 +0300

    Initialized

---
+++ __init__.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+import serial
+import time 
+
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1) #Port name might vary in different OS 
+time.sleep(2) 
+
+model = YOLO("runs/detect/train/weights/best.pt") #Trained data
+results = model.predict(source=0, show=True, stream=True, conf=0.5) #Camera
+
+last_confirmed_state = '0'
+candidate_state = '0'
+consecutive_count = 0
+CONFIRMATION_THRESHOLD = 10
+last_sent = '0'
+
+for r in results:
+    
+    detected_classes = [model.names[int(c)] for c in r.boxes.cls]
+      
+    current_target = None
+    if 'Gear' in detected_classes: current_target = '1'
+    elif 'Bolt' in detected_classes: current_target = '2'
+    elif 'Nut' in detected_classes: current_target = '3'
+    elif 'Bearing' in detected_classes: current_target = '4'
+    else: current_target = '0'
+
+    if current_target == candidate_state:
+        consecutive_count += 1
+    else:
+        candidate_state = current_target
+        consecutive_count = 0
+
+    if consecutive_count >= CONFIRMATION_THRESHOLD:
+        if candidate_state != last_confirmed_state:
+            arduino.write(candidate_state.encode())
+            print(f"State: {candidate_state}")
+            last_confirmed_state = candidate_state
+
+   
---
+++ sketch_mar23a/sketch_mar23a.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+Servo myServo;
+unsigned long actionStartTime = 0;
+const int actionDuration = 500; 
+bool isMoving = false;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(A0); //Servomotor Pin ID
+  myServo.write(0);
+}
+
+void loop() {
+  
+  if (Serial.available() > 0 && !isMoving) {
+    char command = Serial.read();
+    
+    int blinks = command - '0'; 
+    for(int i = 0; i < blinks; i++) {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+    }
+
+    if (command == '1') myServo.write(45);
+    else if (command == '2') myServo.write(90);
+    else if (command == '3') myServo.write(135);
+    else if (command == '4') myServo.write(180);
+    else if (command == '0') myServo.write(0);
+    
+    while(Serial.available() > 0) Serial.read(); 
+  }
+
+}
